998c8bedcbe0e3643075db6c475a180a973d2701
geovaniprodata <geovani.web13@gmail.com>
2023-02-16 13:35:29 -0300

    gfdgfh

--- lib/src/drop_down.dart
+++ lib/src/drop_down.dart
@@ -394,37 +394,44 @@ class _MainBodyState extends State<MainBody> {
             ),
             Visibility(
               visible: widget.dropDown.confirmarBtn && selected.isInitialized,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                      'SELECIONADO : ${selected.isInitialized ? selected.val.name : 'Carregando..'}'),
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      Icons.restart_alt_rounded,
-                      color: Colors.white,
-                    ),
-                    label: const Text(
-                      'LIMPAR',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll(Colors.grey.shade400),
-                      elevation: const MaterialStatePropertyAll(2),
-                      shape: MaterialStatePropertyAll(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            side: BorderSide.none),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('SELECIONADO : '),
+                    Text(
+                        selected.isInitialized
+                            ? selected.val.name
+                            : 'Carregando..',
+                        softWrap: true),
+                    ElevatedButton.icon(
+                      icon: const Icon(
+                        Icons.restart_alt_rounded,
+                        color: Colors.white,
                       ),
+                      label: const Text(
+                        'LIMPAR',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll(Colors.grey.shade400),
+                        elevation: const MaterialStatePropertyAll(2),
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              side: BorderSide.none),
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          selected = Late();
+                        });
+                      },
                     ),
-                    onPressed: () {
-                      setState(() {
-                        selected = Late();
-                      });
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
